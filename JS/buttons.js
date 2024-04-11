c336927995b5a7d1b69591e67487419b81adef6f
ivan-carballo <ivanrallyasfalto@hotmail.com>
2024-04-11 13:26:34 +0200

    Explicacion HASH MD5 en Readme

--- JS/buttons.js
+++ JS/buttons.js
@@ -11,16 +11,41 @@ const buttonComic = document.getElementById('btn-comic');
 
 
 // Añadir las funciones de llamada de API
-async function llamarAPI () {
-    //
+async function getMarvels() {
+    const url = new URL("https://gateway.marvel.com:443/v1/public/characters?apikey=295966637e5c92693cf21758d4481539");
+    url.searchParams.append("name", inputCharacter.textContent);
+    url.searchParams.append('hash', '734e4e50451108fbdb91a833017b97d90ea9eac5')
+    try {
+        const response = await fetch(url.toString());
+        const data = await response.json();
+        console.log(data);
+        alert(data);
+    }
+    catch (error) {
+        console.error(error);
+        alert(error);
+    }
 }
+
+async function getMarvel(url) {
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        console.log(data);
+    }
+    catch (error) {
+        console.error(error)
+    }
+}
+
 
 
 // Usar un escuchador de eventos cuando se hace click en alguno de los dos botones
 // Dentro del escuchador de eventos se añade una funcion flecha con lo que hara al ser pulsado
 buttonCharacter.addEventListener('click', (e) => {
     if (inputCharacter.value.length > 0) {
-        alert(`Estas buscando informacion sobre ${inputCharacter.value}, lamentablemente la API aun esta en construccion y no hay informacion para mostrarte`);
+        //alert(`Estas buscando informacion sobre ${inputCharacter.value}, lamentablemente la API aun esta en construccion y no hay informacion para mostrarte`);
+        getMarvels();
     } else {
         alert(`Lamentamos informar que la API aun esta en construccion y no hay informacion para mostrarte`);
     }
