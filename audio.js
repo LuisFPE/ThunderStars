c5c882e81c0ce42ba85aadef85bd7569f14698c9
cristobendito <flopybbb@gmail.com>
2024-04-18 15:14:54 +0200

    cambios

--- audio.js
+++ audio.js
@@ -1,4 +1,4 @@
-    document.onmousemove = function() {
+/*     document.onmousemove = function() {
         playAudio();
         document.onmousemove = null;
     };
@@ -6,4 +6,42 @@
     function playAudio() {
         var audio = document.getElementById('my_audio');
         audio.play();
+    } */
+/* 
+    var audioPlayed = false;
+
+document.onmousemove = function() {
+    if (!audioPlayed) {
+        playAudio();
+        audioPlayed = true;
     }
+};
+
+function playAudio() {
+    var audio = document.getElementById('my_audio');
+    audio.play();
+}
+ */
+
+var audioPlayed = false;
+var context = null;
+
+document.onmousemove = function() {
+    if (!audioPlayed && isAutoplayAllowed()) {
+        playAudio();
+        audioPlayed = true;
+    }
+};
+
+function isAutoplayAllowed() {
+    // Verificar si el navegador permite la reproducción automática
+    if (context === null) {
+        context = new AudioContext();
+    }
+    return context.state === 'running';
+}
+
+function playAudio() {
+    var audio = document.getElementById('my_audio');
+    audio.play();
+}
